6b09594711298903b33b59e5c71265dea4199aa3
ErikReiz <mihailkrestin1@gmail.com>
2025-03-19 12:29:50 +0100

    Initial commit

---
+++ src/main/java/com/cmasproject/cmastestserver/CmasTestServerApplication.java
@@ -0,0 +1,14 @@
+package com.cmasproject.cmastestserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmasTestServerApplication {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(CmasTestServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cmasproject/cmastestserver/CmasTestServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.cmasproject.cmastestserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmasTestServerApplicationTests {
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
